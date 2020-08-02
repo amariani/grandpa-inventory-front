0d1645cf22c3fabc8159c1eae50be7eda5aa30c3
Ariel Mariani <ariel@peermedical.com>
2020-08-02 04:12:00 -0300

    Add _app and _component files Add base components to test emotion and polished libraries.

---
+++ components/Header.js
@@ -0,0 +1,34 @@
+// import React from 'react';
+import styled from '@emotion/styled';
+import { rem } from 'polished';
+
+function Header({ isDark }) {
+  return (
+    <HeaderStyled isDark={isDark}>
+      <div className="container">
+        <div className="logo">
+          <img src="/images/inventory-logo.png" alt="Logo" />
+          <span className="logo-text">Grandpa Inventory</span>
+        </div>
+      </div>
+    </HeaderStyled>
+  );
+}
+
+const HeaderStyled = styled.header`
+  background: ${(props) =>
+    props.isDark ? props.theme.colors.primary : props.theme.colors.secondary};
+  padding: 20px;
+  .logo {
+    display: flex;
+    align-items: center;
+    .logo-text {
+      color: #333;
+      font-weight: bold;
+      font-size: ${rem(20)};
+      margin-left: 20px;
+    }
+  }
+`;
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import Header from '../components/Header';
+import { ThemeProvider } from 'emotion-theming';
+import GlobalStyles from '../components/GlobalStyles/GlobalStyles';
+
+const theme = {
+  colors: {
+    primary: '#0f0',
+    secondary: '#00f',
+  },
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Header isDark />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
